ebc7f03a4bfd0765992baf97be29dc383dfdb9f0
unknown <mohamed.ashraf.rady0@gmail.com>
2022-06-20 02:16:07 +0200

    login screen designing

--- app/src/main/java/com/moashrafff/nectar/MainActivity.kt
+++ app/src/main/java/com/moashrafff/nectar/MainActivity.kt
@@ -2,10 +2,22 @@ package com.moashrafff.nectar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.moashrafff.nectar.views.pages.SplashFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_on_boarding)
+        setContentView(R.layout.activity_main)
+        showFragment(SplashFragment())
+
     }
+
+
+    private fun showFragment (fragment : Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container,fragment)
+            .commit()
+     }
 }
--- app/src/main/java/com/moashrafff/nectar/views/pages/SplashFragment.kt
+++ app/src/main/java/com/moashrafff/nectar/views/pages/SplashFragment.kt
@@ -1,34 +1,28 @@
 package com.moashrafff.nectar.views.pages
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.moashrafff.nectar.R
-import com.moashrafff.nectar.databinding.FragmentOnBoardingBinding
 
+class SplashFragment : Fragment() {
 
-class OnBoardingFragment : Fragment() {
-
-    private var _binding: FragmentOnBoardingBinding? = null
-    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding?.getStartedBtn?.setOnClickListener(View.OnClickListener {
-
-            showFragment(LoginFragment())
-
-        })
+        Handler().postDelayed(Runnable {
+            showFragment(OnBoardingFragment())
+        },3000)
     }
 
     private fun showFragment (fragment : Fragment){
@@ -36,11 +30,5 @@ class OnBoardingFragment : Fragment() {
             ?.replace(R.id.container,fragment)
             ?.commit()
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 
 }
