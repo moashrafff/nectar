d931ae91473682b76d1c2b232cc0e46d957f5db8
Mohamed Ashraf <85141514+moashrafff@users.noreply.github.com>
2022-10-27 01:57:48 +0200

    connecting firebase with the project & Implementing authentication with email and password

---
+++ app/src/main/java/com/moashrafff/nectar/config/Constants.kt
@@ -0,0 +1,11 @@
+package com.moashrafff.nectar.config
+
+class Constants {
+
+    companion object {
+        const val USER_USERNAME_KEY = "username"
+        const val USER_EMAIL_KEY = "email"
+        const val USER_PASSWORD_KEY = "password"
+    }
+
+}
--- app/src/main/java/com/moashrafff/nectar/views/pages/b_Account/a_sign_up/SignUpFragment.kt
+++ app/src/main/java/com/moashrafff/nectar/views/pages/b_Account/a_sign_up/SignUpFragment.kt
@@ -1,14 +1,31 @@
 package com.moashrafff.nectar.views.pages.b_Account.a_sign_up
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.moashrafff.nectar.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.moashrafff.nectar.config.Constants
+import com.moashrafff.nectar.databinding.FragmentSignUpBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class SignUpFragment : Fragment() {
+
+    private val binding by lazy { FragmentSignUpBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    val user: HashMap<String, Any> = HashMap()
 
 
     override fun onCreateView(
@@ -16,7 +33,73 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        db = FirebaseFirestore.getInstance()
+
+        binding.signupBtn.setOnClickListener(View.OnClickListener {
+            registerUser()
+            binding.loginEmailEt.text?.clear()
+            binding.loginPassEt.text?.clear()
+            binding.regNameEt.text?.clear()
+
+        })
+
+    }
+
+    private fun registerUser() {
+        val email = binding.loginEmailEt.text.toString()
+        val password = binding.loginPassEt.text.toString()
+        val username = binding.regNameEt.text.toString()
+
+        user[Constants.USER_USERNAME_KEY] = username
+        user[Constants.USER_EMAIL_KEY] = email
+        user[Constants.USER_PASSWORD_KEY] = password
+
+        if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty()) {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    saveUserIntoFireBase()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                    }
+
+
+                }
+            }
+        }
+
+
+    }
+
+
+    private fun checkLoggedState() {
+        if (auth.currentUser == null) {
+            Toast.makeText(requireContext(), "You are not logged", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun saveUserIntoFireBase() {
+        db.collection("user").add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.e("", "Login success")
+            }
+            .addOnFailureListener { e ->
+                Log.e("", "Login fail")
+            }
+    }
+
 
 }
